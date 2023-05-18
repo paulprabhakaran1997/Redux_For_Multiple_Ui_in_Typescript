0365e6293e29cbf5703e2fb15b4afd1da2d6be30
Paul Prabhakaran <paul@lintcloud.com>
2023-05-18 17:26:27 +0530

    Project first Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import UserList from "./components/UserList/UserList";
+import EditUser from "./components/EditUser/EditUser";
+import { useAppDispatch } from "./custom/reduxHooks";
+import { apiGetUser } from "./api/apiGetUser";
 
 function App() {
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    console.log("Project App.js Running");
+    apiGetUser(dispatch)
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/editUser/:id" element={<EditUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
